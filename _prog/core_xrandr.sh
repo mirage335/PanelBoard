e7167f28e7e709ef72adb5d9328643158e356bc7
mirage335 <spamfreemirage335@gmail.com>
2023-09-10 05:03:39 -0400

    Draft.

--- _prog/core_xrandr.sh
+++ _prog/core_xrandr.sh
@@ -7,21 +7,28 @@ _xrandr_change_hash() {
 
 _xrandr_primary_string() {
 	#https://unix.stackexchange.com/questions/409650/how-to-get-monitor-width-using-xrandr-query
-	xrandr --query | sed 's/default/primary/' | grep 'primary'
+	#xrandr --query | sed 's/default/primary/' | grep 'primary'
+	xrandr --query | grep 'primary\|default'
 }
 
 # ATTENTION: Override with 'ops' if necessary.
 # DANGER: Unstable format.
 # DANGER: Expected not to be available under wayland.
 _xrandr_primary_string_extract_width() {
-	awk -F '[ x+]' '/\<primary\>/{print $4}' | tr -dc '0-9\-'
+	#awk -F '[ x+]' '/\<primary\>/{print $4}' | tr -dc '0-9\-'
+	
+	# ATTRIBUTION: ChatGPT-4 2023-09-10 (for awk script).
+	awk -F '[x+]' '{print $1}' | sed 's/.* \([0-9]*\)[^0-9]*$/\1/'
 }
 
 # ATTENTION: Override with 'ops' if necessary.
 # DANGER: Unstable format.
 # DANGER: Expected not to be available under wayland.
 _xrandr_primary_string_extract_height() {
-	awk -F '[ x+]' '/\<primary\>/{print $5}' | tr -dc '0-9\-'
+	#awk -F '[ x+]' '/\<primary\>/{print $5}' | tr -dc '0-9\-'
+	
+	# ATTRIBUTION: ChatGPT-4 2023-09-10 (for awk script).
+	awk -F '[x+]' '{print $2}' | sed 's/.* \([0-9]*\)[^0-9]*$/\1/'
 }
 
 
--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -36,7 +36,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='2591634041'
-export ub_setScriptChecksum_contents='4055925684'
+export ub_setScriptChecksum_contents='1967080560'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -20018,21 +20018,28 @@ _xrandr_change_hash() {
 
 _xrandr_primary_string() {
 	#https://unix.stackexchange.com/questions/409650/how-to-get-monitor-width-using-xrandr-query
-	xrandr --query | sed 's/default/primary/' | grep 'primary'
+	#xrandr --query | sed 's/default/primary/' | grep 'primary'
+	xrandr --query | grep 'primary\|default'
 }
 
 # ATTENTION: Override with 'ops' if necessary.
 # DANGER: Unstable format.
 # DANGER: Expected not to be available under wayland.
 _xrandr_primary_string_extract_width() {
-	awk -F '[ x+]' '/\<primary\>/{print $4}' | tr -dc '0-9\-'
+	#awk -F '[ x+]' '/\<primary\>/{print $4}' | tr -dc '0-9\-'
+	
+	# ATTRIBUTION: ChatGPT-4 2023-09-10 (for awk script).
+	awk -F '[x+]' '{print $1}' | sed 's/.* \([0-9]*\)[^0-9]*$/\1/'
 }
 
 # ATTENTION: Override with 'ops' if necessary.
 # DANGER: Unstable format.
 # DANGER: Expected not to be available under wayland.
 _xrandr_primary_string_extract_height() {
-	awk -F '[ x+]' '/\<primary\>/{print $5}' | tr -dc '0-9\-'
+	#awk -F '[ x+]' '/\<primary\>/{print $5}' | tr -dc '0-9\-'
+	
+	# ATTRIBUTION: ChatGPT-4 2023-09-10 (for awk script).
+	awk -F '[x+]' '{print $2}' | sed 's/.* \([0-9]*\)[^0-9]*$/\1/'
 }
 
 
